b76fb78ca076498078bb94e34c2ec42d2a47f416
Tam Nguyen <tamna@techvify.com.vn>
2021-09-15 13:38:07 +0700

    feat: sign-in and sign-up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { Connection } from 'typeorm'
+import { AuthService } from './auth.service'
+import { SignUpDto } from './dtos'
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService, private configService: ConfigService) {}
+
+  @Post('sign-up')
+  async signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto)
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common'
+import { SignUpDto } from './dtos'
+import { scrypt, createCipheriv } from 'crypto'
+import { promisify } from 'util'
+import { Connection } from 'typeorm'
+import { User } from 'src/entities'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private connection: Connection) {}
+  private async cipherPassword(password: string): Promise<string> {
+    const iv = Buffer.from('VIETNAM-TECHVIFY')
+    const salt = '$@$tNRnC5qdHZXBO$$*4'
+    const keyLength = 32
+
+    const key = (await promisify(scrypt)(password, salt, keyLength)) as Buffer
+    const cipher = createCipheriv('aes-256-gcm', key, iv)
+
+    const secret = 'TECHVIFY'
+    const encryptedPassword = Buffer.concat([cipher.update(secret), cipher.final()]).toString('hex')
+
+    return encryptedPassword
+  }
+
+  async signUp(dto: SignUpDto): Promise<Partial<User>> {
+    const manager = this.connection.manager
+
+    const encryptedPassword = await this.cipherPassword(dto.password)
+
+    const userDto: Partial<User> = {
+      email: dto.email,
+      password: encryptedPassword,
+    }
+
+    const userEntity = await manager.save(User, userDto)
+
+    const { password, ...rest } = await manager.findOne(User, { id: userEntity.id })
+    return rest
+  }
+
+  async signIn(dto: SignUpDto): Promise<Partial<User>> {
+    const manager = this.connection.manager
+
+    const encryptedPassword = await this.cipherPassword(dto.password)
+
+    const userEntity = await manager.findOne(User, { email: dto.email, password: encryptedPassword })
+
+    if (!userEntity) {
+      throw new BadRequestException('Your account does not exist')
+    }
+
+    const { password, ...rest } = userEntity
+
+    return rest
+  }
+}
