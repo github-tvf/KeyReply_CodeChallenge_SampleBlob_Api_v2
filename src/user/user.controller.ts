e7aa4999f6abf45adce2a59256504313e51fcb7a
Tam Nguyen <tamna@techvify.com.vn>
2021-09-18 09:30:29 +0700

    feat: user module

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common'
+import { AuthGuard } from 'src/auth/guards'
+import { CurrentUser } from 'src/decorators/current-user.decorator'
+import { Blog, User } from 'src/entity'
+import { UserService } from '.'
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get('')
+  async getAll(): Promise<null> {
+    return null
+  }
+
+  @Get(':id/blogs')
+  @UseGuards(AuthGuard)
+  async getAllBlogsOfUser(@CurrentUser() user: User): Promise<Blog[]> {
+    return this.userService.getAllBlogsOfUser(user)
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common'
-import { User } from 'src/entity'
+import { Blog, User } from 'src/entity'
 import { Connection } from 'typeorm'
 
 @Injectable()
@@ -7,5 +7,17 @@ export class UserService {
   constructor(private connection: Connection) {}
   async findUser(email: string, password: string): Promise<Partial<User>> {
     return this.connection.manager.findOne(User, { email, password })
+  }
+
+  async getAllBlogsOfUser(user: User): Promise<Blog[]> {
+    const blogs = await this.connection.manager
+      .createQueryBuilder(Blog, 'blog')
+      .leftJoinAndSelect('blog.user', 'user')
+      .andWhere('user.id = :userId', { userId: user.id })
+      .select()
+      .getMany()
+
+    blogs.forEach((blog) => delete blog.user)
+    return blogs
   }
 }
